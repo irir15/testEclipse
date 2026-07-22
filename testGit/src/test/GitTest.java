0f8bfaa3d322d29e6d28d2af0c40847c93f9c1b1
irir15 <nasunassun321@gmail.com>
2026-07-22 14:03:40 +0900

    GitTest.java Update

--- testGit/src/test/GitTest.java
+++ testGit/src/test/GitTest.java
@@ -2,7 +2,6 @@ package test;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Hello");
 		System.out.println("Good Night GitHub!!");
 		System.out.println("Hello GitHub!!");
 		System.out.println("Good Night GitHub!!");
