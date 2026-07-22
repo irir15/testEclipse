806b4bd12fff51c41c0b84133629df2e8ca335a1
irir15 <nasunassun321@gmail.com>
2026-07-22 14:25:27 +0900

    Add duplicate 'Hello Linux' print statement

--- testGit/src/test/LinuxTest.java
+++ testGit/src/test/LinuxTest.java
@@ -4,5 +4,6 @@ public class LinuxTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Linux");
 		System.out.println("GoodNight Linux");
+		System.out.println("Hello Linux");
 	}
 }
